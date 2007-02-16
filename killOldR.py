57b4b08626ce1654b3f8fd2ffe5f711c8abca077
RDU <rdiaz@ligarto.org>
2007-02-16 17:12:09 +0100

    added killolr and tryrrun4

---
+++ killOldR.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Kills lamds and Rs longer than 24 hours run by the user in charge of the we app.
+we use an ugly hack: bsdstart returns the duration of a process as HH:MM only if
+less < 24. Thus, we just check for the precessend of ':' in the given field."""
+
+
+import os
+
+## Read configuration options
+# from ConfigParser import ConfigParser
+# config = ConfigParser()
+# config.read('../../asterias_config2.ini')
+# try:
+#     USER = eval(config.get('MPI_config', 'USER'))
+# except:
+#     print 'USER ([MPI_config] in asterias_config2.ini) evaluation error. \
+#     It should be a (Python) string.'
+
+USER = 'www-data'
+
+
+Rs_running    = os.popen('ps -C R -o pid,user,bsdstart h').readlines()
+
+for proc_line in Rs_running:
+    proc_break = proc_line.strip().split()
+    if (proc_break[1] == USER) and (proc_break[2].find(':') == -1):
+	os.system('kill -s 9 ' + str(int(proc_break[0])))
+
---
+++ tryRrun4.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+
+"""
+Now using a single, system-wide LAM universe. But nodes are then
+always used in the same order. Unless there are as many universes as
+master nodes. Or we modify the mpi.spawn.Rslaves to rotate the slaves
+used, like in the mpi.defs file.
+
+"""
+
+import os
+import time
+import signal
+import shutil
+import sys
+import socket
+import counterApplications
+import random
+tmpDir = sys.argv[1]
+application = sys.argv[2]
+
+
+numtries = 20 ## I redefine it here. for really stubborn cases
+
+
+def collectZombies(k = 10):
+    """ Make sure there are no zombies in the process tables.
+    This is probably an overkill, but works.
+    """
+    for nk in range(k):
+        try:
+            tmp = os.waitpid(-1, os.WNOHANG)
+        except:
+            None
+
+
+## simpler if these two are cron jobs; this way, the appl. is faster.
+#os.system("cd " + tmpDir + "; /http/mpi.log/buryThem.py")
+#killedlamandr = os.system('/http/mpi.log/killOldLam.py')
+
+try:
+    counterApplications.add_to_log(application, tmpDir, socket.gethostname())
+except:
+    None
+
+startedOK = False
+## time.sleep(random.uniform(0.5, 8)) ## to prevent truly simultaneous from crashing MPI. Unneeded now
+
+
+
+for i in range(int(numtries)):
+    fullRcommand = 'cd ' + tmpDir + \
+                   '; sleep 1; /usr/local/R-custom/bin/R  --no-restore --no-readline --no-save --slave <f1.R >>f1.Rout 2> error.msg &'
+    Rrun = os.system(fullRcommand)
+    time.sleep(40)
+    collectZombies()
+
+    if os.path.exists(tmpDir + "/mpiOK"):
+        startedOK = True
+        break
+        
+    if not os.path.exists('/http/mpi.log/' + application + 'ErrorLog'):
+        os.system('touch /http/mpi.log/' + application + 'ErrorLog')
+    outlog = open('/http/mpi.log/' + application + 'ErrorLog', mode = 'a')
+    outlog.write('MPI fails on ' + time.ctime(time.time()) +
+                 ' Directory: ' + tmpDir + '\n')
+    outlog.close()
+   
+   
+if not startedOK:
+    ## Logging
+    if not os.path.exists('/http/mpi.log/' + application + 'ErrorLog'):
+        os.system('touch /http/mpi.log/' + application + 'ErrorLog')
+    outlog = open('/http/mpi.log/' + application + 'ErrorLog', mode = 'a')
+    outlog.write('MPI fails on ' + time.ctime(time.time()) +
+                 ' Directory: ' + tmpDir + '\n')
+    outlog.close()
+    ## Make sure the checkdone.cgi will stop; we create the two files here
+    ## either of which will lead to loading results.html
+    out1 = open(tmpDir + "/natural.death.pid.txt", mode = "w")
+    out2 = open(tmpDir + "/kill.pid.txt", mode = "w")
+    out1.write('MPI initialization error!!')
+    out2.write('MPI initialization error!!')
+    out1.close()
+    out2.close()
+    outf = open(tmpDir + "/pre-results.html", mode = "w")
+    outf.write("<html><head><title> MPI initialization problem.</title></head><body>\n")
+    outf.write("<h1> MPI initialization problem.</h1>")
+    outf.write("<p> After " + numtries + " attempts we have been unable to ")
+    outf.write(" initialize MPI.</p>")
+    outf.write("<p> We will be notified of this error, but we would also ")
+    outf.write("appreciate if you can let us know of any circumstances or problems ")
+    outf.write("so we can diagnose the error.</p>")
+    outf.write("</body></html>")
+    outf.close()
+    shutil.copyfile(tmpDir + "/pre-results.html", tmpDir + "/results.html")
+
+
